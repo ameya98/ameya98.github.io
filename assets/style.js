4a794e824d74525aa184ef44719149f39346d43c
Ameya Daigavane <ameya.d.98@gmail.com>
2023-09-30 05:18:25 -0400

    Fix.

--- assets/style.js
+++ assets/style.js
@@ -40,7 +40,7 @@ const namefontsize = "23pt";
 
 
 // Publication properties
-const ptitlecolor = "accentcolor";
+const ptitlecolor = accentcolor;
 const ptitlefontsize = bodyfontsize;
 const ptitleweight = 400;
 const ptitledecoration = "none";
